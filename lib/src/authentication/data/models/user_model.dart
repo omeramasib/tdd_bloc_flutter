117a5c55bec1b75bb8f4fad5ed4b4b51ca3ca673
omeramasib <omeramasib999@gmail.com>
2024-05-09 15:57:35 +0300

    add new changes

---
+++ lib/src/authentication/data/models/user_model.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:tdd_flutter/core/utils/typedef.dart';
+import 'package:tdd_flutter/src/authentication/domain/entities/user.dart';
+
+class UserModel extends User {
+  const UserModel({
+    required super.avatar,
+    required super.id,
+    required super.createdAt,
+    required super.name,
+  });
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(jsonEncode(source) as DataMap);
+
+  UserModel.fromMap(DataMap map)
+      : this(
+          avatar: map['avatar'] as String,
+          id: map['id'] as String,
+          createdAt: map['createdAt'] as String,
+          name: map['name'] as String,
+        );
+
+  UserModel copyWith({
+    String? avatar,
+    String? id,
+    String? createdAt,
+    String? name,
+  }) {
+    return UserModel(
+      avatar: avatar ?? this.avatar,
+      id: id ?? this.id,
+      createdAt: createdAt ?? this.createdAt,
+      name: name ?? this.name,
+    );
+  }
+
+  DataMap toMap() => {
+        'id': id,
+        'avatar': avatar,
+        'createdAt': createdAt,
+        'name': name,
+      };
+
+  String toJson() => jsonEncode(toMap());
+}
--- lib/src/authentication/domain/entities/user.dart
+++ lib/src/authentication/domain/entities/user.dart
@@ -1,14 +1,14 @@
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
-  final int? id;
+  final String? id;
   final String? name;
   final String? createdAt;
   final String? avatar;
 
   const User({this.id, this.name, this.createdAt, this.avatar});
 
-  const User.empty() : this(id: 1, createdAt: '_empty.createdAt', name: '_empty.name', avatar: '_empty.avatar');
+  const User.empty() : this(id: '1', createdAt: '_empty.createdAt', name: '_empty.name', avatar: '_empty.avatar');
 
   @override
   List<Object?> get props => [id];
