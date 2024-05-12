25600d3b9271fd4ca1f3622e49948af375d3d62a
omeramasib <omeramasib999@gmail.com>
2024-05-12 14:31:01 +0300

    add new changes

--- lib/src/authentication/data/models/user_model.dart
+++ lib/src/authentication/data/models/user_model.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:tdd_flutter/core/utils/typedef.dart';
 import 'package:tdd_flutter/src/authentication/domain/entities/user.dart';
 
@@ -10,16 +11,24 @@ class UserModel extends User {
     required super.name,
   });
 
+  const UserModel.empty()
+  : this(
+    id: '1',
+    createdAt: '_empty.createdAt',
+    name: '_empty.name',
+    avatar: '_empty.avatar',
+  );
+
   factory UserModel.fromJson(String source) =>
-      UserModel.fromMap(jsonEncode(source) as DataMap);
+    UserModel.fromMap(jsonDecode(source) as DataMap);
 
   UserModel.fromMap(DataMap map)
-      : this(
-          avatar: map['avatar'] as String,
-          id: map['id'] as String,
-          createdAt: map['createdAt'] as String,
-          name: map['name'] as String,
-        );
+    : this(
+        avatar: map['avatar'] as String,
+        id: map['id'] as String,
+        createdAt: map['createdAt'] as String,
+        name: map['name'] as String,
+      );
 
   UserModel copyWith({
     String? avatar,
@@ -28,19 +37,19 @@ class UserModel extends User {
     String? name,
   }) {
     return UserModel(
-      avatar: avatar ?? this.avatar,
-      id: id ?? this.id,
-      createdAt: createdAt ?? this.createdAt,
-      name: name ?? this.name,
+        avatar: avatar ?? this.avatar,
+        id: id ?? this.id,
+        createdAt: createdAt ?? this.createdAt,
+        name: name?? this.name
     );
   }
 
   DataMap toMap() => {
-        'id': id,
-        'avatar': avatar,
-        'createdAt': createdAt,
-        'name': name,
-      };
+    'id': id,
+    'avatar': avatar,
+    'createdAt': createdAt,
+    'name': name,
+  };
 
   String toJson() => jsonEncode(toMap());
 }
