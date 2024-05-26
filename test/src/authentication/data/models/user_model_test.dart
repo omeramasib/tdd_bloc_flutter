c39c5221cdf7f3f18f8586b100309048864d0fba
omeramasib <omeramasib999@gmail.com>
2024-05-26 22:50:33 +0300

    add new changes

--- test/src/authentication/data/models/user_model_test.dart
+++ test/src/authentication/data/models/user_model_test.dart
@@ -1,5 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_flutter/src/authentication/data/models/user_model.dart';
+import 'package:tdd_flutter/src/authentication/domain/entities/user.dart';
 
 void main() {
-  test('UserModel', () {});
+  test('should be a subclass of [User] entity', () {
+    // Arrange
+    const tUserModel = UserModel.empty();
+    // Act
+
+    // Assert
+    expect(tUserModel, isA<User>());
+});
+    group('fromMap', () {
+      test('should return a [UserModel] with the right data', ()  {
+        // Arrange
+        final tJson = File('test/fixures/user.json').readAsStringSync();
+        print(tJson);
+        // Act
+        // final result = UserModel.fromMap(tJson);
+      });
+    });
 }
